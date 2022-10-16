089962d4b97cdde6b264d5ad850e071f870f0aa6
mertsal61 <mertsal92@gmail.com>
2022-10-16 22:32:55 +0300

    evpc

--- Proje_OOP/Program.cs
+++ Proje_OOP/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext();
+builder.Services.AddIdentity<AppUser>
+
 
 var app = builder.Build();
 
